d5a5e6a3c16fc6802ccee66df2777bbbfc35268d
Ohm-Rajpal <ohm_rajpal@berkeley.edu>
2025-03-30 13:19:41 -0700

    BFS implementation

--- graph/BFS.py
+++ graph/BFS.py
@@ -1,1 +1,38 @@
 # BFS
+from collections import deque
+from GraphClass.MyGraph import Graph
+
+def BFS(graph, source):
+    # initialize the queue
+    Q = deque()
+    graph.visited[source] = True
+    print(f'visited {source}')
+    
+    # add vertex
+    Q.append(source)
+
+    while Q: # while the queue is not empty
+        vertex = Q.popleft()
+
+        # add other verticies to the queue
+        for child in graph.getEdges(vertex):
+            if not graph.visited[child]:
+                graph.visited[child] = True
+                print(f'visited {child}')
+                Q.append(child)
+    
+def main():
+    graph = Graph({
+        "a": ["b", "c"],
+        "b": ["a", "d", "e"],
+        "c": ["a", "f"],
+        "d": [],
+        "e": ["f"],
+        "f": [],
+        "g": ["f"]
+    })
+
+    BFS(graph, "a")
+
+if __name__ == "__main__":
+    main()
