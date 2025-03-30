29a6831e6452ba3f5c6843109c0f723466fafcb4
Ohm-Rajpal <ohm_rajpal@berkeley.edu>
2025-03-30 16:51:23 -0700

    Bottom up longest increasing subsequence

--- DynamicProgramming/inc_subseq.py
+++ DynamicProgramming/inc_subseq.py
@@ -1,1 +1,17 @@
 # largest increasing subsequence
+def longest_subseq(arr):
+    dp = [1] * len(arr)
+
+    for i in range(len(arr)):
+        for j in range(0, i):
+            if arr[j] > arr[i]:
+                dp[i] = max(dp[i], 1 + dp[j])
+
+    return max(dp)
+
+def main():
+    arr = [5, 2, 8, 6]
+    print(longest_subseq(arr))
+
+if __name__ == "__main__":
+    main()
